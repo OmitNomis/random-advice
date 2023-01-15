fcb1eec2ca8e71fe48f1afbe8632991daf3c417c
OmitNomis <simon98190@gmail.com>
2023-01-15 18:42:57 +0545

    remove app.css, remove all code from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
