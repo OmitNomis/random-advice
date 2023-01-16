08582f1541d7cd60934562d62ec52a699db31119
OmitNomis <simon98190@gmail.com>
2023-01-16 12:39:19 +0545

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Background from "./assets/large.jfif";
+import Background from "./assets/images/background.jfif";
+import { motion } from "framer-motion";
 
 function App() {
   const [showAdvice, setShowAdvice] = useState(false);
@@ -17,22 +18,45 @@ function App() {
         />
 
         <div className="flex flex-col items-center justify-center min-h-[100vh]s text-[#2D0EC6] text-[22px] z-10">
-          <h1 className="font-semibold mb-2 text-[24px]">
+          <motion.h1
+            initial={{ y: "-100vh", opacity: 0.5 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="font-semibold mb-2 text-[24px]"
+          >
             Feeling unsure? Let us give you a questionable decision.
-          </h1>
-          <h2 className=" mb-2">Advice so bad, it’s good.</h2>
-          <h2 className=" mb-11">
+          </motion.h1>
+          <motion.h2
+            initial={{ y: "-100vh", opacity: 0.5 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            className=" mb-2"
+          >
+            Advice so bad, it’s good.
+          </motion.h2>
+          <motion.h2
+            initial={{ y: "-100vh", opacity: 0.5 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2 }}
+            className=" mb-11"
+          >
             Ready for taste of mediocrity? Press the button for a little
             surprise.
-          </h2>
+          </motion.h2>
 
-          <button className="text-white bg-[#2D0EC6] p-1 px-3 rounded-[8px] flex justify-center items-center">
+          <motion.button
+            initial={{ y: "-100vh", opacity: 0.5 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 3 }}
+            className="text-white bg-[#2D0EC6] p-1 px-3 rounded-[8px] flex justify-center items-center"
+          >
             <p>Why not?</p>
-          </button>
+          </motion.button>
         </div>
       </div>
-
-      <div className="min-h-[100vh] w-full flex flex-col justify-center items-center"></div>
+      {showAdvice === true && (
+        <div className="min-h-[100vh] w-full flex flex-col justify-center items-center"></div>
+      )}
     </>
   );
 }
