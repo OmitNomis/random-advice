5e18745da41bdfce518ace1b24253526e7de1c7d
OmitNomis <simon98190@gmail.com>
2023-01-16 12:52:00 +0545

    first page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,23 @@
 import React, { useState } from "react";
 import Background from "./assets/images/background.jfif";
-import { motion } from "framer-motion";
-import Button from "./components/Button";
+import FirstPage from "./components/FirstPage";
 
 function App() {
   const [showAdvice, setShowAdvice] = useState(false);
 
   return (
     <>
-      <div className=" min-h-[100vh] w-full flex flex-col justify-center items-center">
-        <img
-          src={Background}
-          alt=""
-          onContextMenu={(e) => e.preventDefault()}
-          onDragStart={(e) => e.preventDefault()}
-          draggable={false}
-          className="fixed h-full w-full opacity-50 -z-1"
-        />
+      <img
+        src={Background}
+        alt=""
+        onContextMenu={(e) => e.preventDefault()}
+        onDragStart={(e) => e.preventDefault()}
+        draggable={false}
+        className="fixed h-full w-full opacity-50 -z-1"
+      />
 
-        <div className="flex flex-col items-center justify-center min-h-[100vh]s text-[#2D0EC6] text-[22px] z-10">
-          <motion.h1
-            initial={{ y: "-100vh", opacity: 0.5 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1 }}
-            className="font-semibold mb-2 text-[24px]"
-          >
-            Feeling unsure? Let us give you a questionable decision.
-          </motion.h1>
-          <motion.h2
-            initial={{ y: "-100vh", opacity: 0.5 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className=" mb-2"
-          >
-            Advice so bad, it’s good.
-          </motion.h2>
-          <motion.h2
-            initial={{ y: "-100vh", opacity: 0.5 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 2 }}
-            className=" mb-11"
-          >
-            Ready for taste of mediocrity? Press the button for a little
-            surprise.
-          </motion.h2>
-          <motion.div
-            initial={{ y: "-100vh", opacity: 0.5 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            <Button title={"Why not?"} />
-          </motion.div>
-        </div>
-      </div>
-      {showAdvice === true && (
-        <div className="min-h-[100vh] w-full flex flex-col justify-center items-center"></div>
-      )}
+      {showAdvice === false ? <FirstPage /> : <SecondPage />}
+      <FirstPage />
     </>
   );
 }
---
+++ src/components/FirstPage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Button from "./Button";
+
+function FirstPage() {
+  return (
+    <div className=" min-h-[100vh] w-full flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center justify-center min-h-[100vh]s text-[#2D0EC6] text-[22px] z-10">
+        <motion.h1
+          initial={{ y: "-100vh", opacity: 0.5 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="font-semibold mb-2 text-[24px]  w-full text-center"
+        >
+          Feeling unsure? Let us give you a questionable decision.
+        </motion.h1>
+        <motion.h2
+          initial={{ y: "-100vh", opacity: 0.5 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          className=" mb-2 w-full text-center"
+        >
+          Advice so bad, it’s good.
+        </motion.h2>
+        <motion.h2
+          initial={{ y: "-100vh", opacity: 0.5 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 2 }}
+          className=" mb-11 w-full text-center"
+        >
+          Ready for taste of mediocrity? Press the button for a little surprise.
+        </motion.h2>
+        <motion.div
+          initial={{ y: "-100vh", opacity: 0.5 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 3 }}
+        >
+          <Button title={"Why not?"} />
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+export default FirstPage;
