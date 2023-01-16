5c50622fd3b6cd3f77c8350236c896b30c8799fe
OmitNomis <simon98190@gmail.com>
2023-01-16 18:22:29 +0545

    responsive changes

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -14,13 +14,13 @@ function SecondPage({ toggleScreen, advice, newAdvice }) {
       }}
       transition={{ duration: 0.5 }}
       exit={{ y: "100vh", opacity: 0 }}
-      className=" min-h-[100vh] w-full flex flex-col justify-center items-center"
+      className=" min-h-[100vh] w-full flex flex-col justify-center items-center px-5"
     >
-      <div className="flex flex-col items-center justify-center min-h-[100vh]s text-[#2D0EC6] text-[22px] z-10">
-        <h1 className="font-semibold mb-2 text-[24px]  w-full text-center">
+      <div className="flex flex-col items-center justify-center min-h-[100vh] text-[#2D0EC6] text-[16px] sm:text-[22px] z-10">
+        <h1 className="font-semibold mb-2 text-[18px] sm:text-[24px]  w-full text-center">
           {advice}
         </h1>
-        <h2 className=" mb-2 text-[18px] w-full text-center">
+        <h2 className=" mb-2 text-[14px] sm:text-[18px] w-full text-center">
           Think this advice is 'meh'? Test your luck again!
         </h2>
         <div>
